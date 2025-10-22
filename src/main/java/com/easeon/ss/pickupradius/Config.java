d67bf4a0a6a81c9228e0f2b344ca6fafd02e1eba
Teron <214763666+EaseonKit@users.noreply.github.com>
2025-10-23 00:01:24 +0900

    1.0.5

--- src/main/java/com/easeon/ss/pickupradius/Config.java
+++ src/main/java/com/easeon/ss/pickupradius/Config.java
@@ -3,7 +3,7 @@ package com.easeon.ss.pickupradius;
 import com.easeon.ss.core.api.common.EaseonConfig;
 
 public class Config extends EaseonConfig {
-    public static final String CONFIG_PATH = String.format("config/easeon/%s.json", Easeon.info.packageName);
+    public static final String CONFIG_PATH = String.format("config/easeon/%s.json", Easeon.info.configName);
 
     public int pickupRadius = 5;
     public int requiredOpLevel = 2;
