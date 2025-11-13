ddaff26784f127bd63f9fd5712035d7d92a1a7c6
Sean-localley <214763666+EaseonKit@users.noreply.github.com>
2025-11-13 11:42:41 +0900

    1.0.0.11

--- src/main/java/com/easeon/ss/pickupradius/EaseonServerTickHandler.java
+++ src/main/java/com/easeon/ss/pickupradius/EaseonServerTickHandler.java
@@ -4,14 +4,12 @@ import com.easeon.ss.core.wrapper.EaseonPlayer;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Box;
 
 public class EaseonServerTickHandler {
     public static void onServerTick(MinecraftServer server, int radius) {
-        for (var player : server.getPlayerManager().getPlayerList()) {
+        for (var player : server.getPlayerManager().getPlayerList())
             handlePlayerPickup(new EaseonPlayer(player), radius);
-        }
     }
 
     private static void handlePlayerPickup(EaseonPlayer player, int radius) {
@@ -22,16 +20,13 @@ public class EaseonServerTickHandler {
         );
 
         for (var item : world.getEntitiesByClass(ItemEntity.class, range, entity -> !entity.cannotPickup())) {
-            if (player.squaredDistanceTo(item) <= radius * radius) {
+            if (player.squaredDistanceTo(item) <= radius * radius)
                 item.onPlayerCollision(player.get());
-            }
         }
 
         for (var xpOrb : world.getEntitiesByClass(ExperienceOrbEntity.class, range, entity -> true)) {
-            if (player.squaredDistanceTo(xpOrb) <= radius * radius) {
+            if (player.squaredDistanceTo(xpOrb) <= radius * radius)
                 xpOrb.onPlayerCollision(player.get());
-            }
         }
     }
-
 }
